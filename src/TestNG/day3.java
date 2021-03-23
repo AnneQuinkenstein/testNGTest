6babb89dc23817020833c1bdf55b4ed7336f946a
Anne Quinkenstein <a.quinkenstein@gmail.com>
2021-03-23 17:59:13 +0100

    added listeners

--- src/TestNG/day3.java
+++ src/TestNG/day3.java
@@ -1,15 +1,17 @@
 package TestNG;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class day3 {
 
     @Test(groups ={"smoke"})
-    public void Demo(){
+    public void DemoFromTest3(){
         System.out.println("hello3");
+        Assert.assertTrue(false);
     }
 
-    @Test(dependsOnMethods = {"Demo"})
+    @Test(dependsOnMethods = {"DemoFromTest3"})
     public void SecondTest(){
         System.out.println("ciao3");
     }
